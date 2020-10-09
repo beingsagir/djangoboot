f8d566a4e2bcfb53114dafa52127b9abe92fde4a
Sagiruddin Mondal <sagir.sagir@gmail.com>
2020-10-09 16:13:07 +0530

    Initial server with basic model and database acess | https://github.com/beingsagir/djangoboot.git

---
+++ myapi/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+
+from .serializers import EmployeeSerializer
+from .models import Employee
+# Create your views here.
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all().order_by('name')
+    serializer_class = EmployeeSerializer
